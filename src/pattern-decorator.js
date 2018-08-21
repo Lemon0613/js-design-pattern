d03d9bf0aa22ff998072759e19551d6ec00719d0
lemon@163.com <lemon@163.com>
2018-08-21 20:34:21 +0800

    decorator

---
+++ src/pattern-decorator.js
@@ -0,0 +1,25 @@
+class Circle {
+  draw () {
+    console.log('draw a circle')
+  }
+}
+
+class Decorator {
+  constructor (cirle) {
+    this.cirle = circle
+  }
+  draw () {
+    this.cirle.draw()
+    this.setRedBorder(cirle)
+  }
+  setRedBorder () {
+    console.log('set a red border')
+  }
+}
+
+// test
+let circle = new Circle()
+circle.draw()
+
+let dec = new Decorator()
+dec.draw()
